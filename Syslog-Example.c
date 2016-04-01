97bd76772d5e5fb91c0866ea47eebd9789f6f9a7
MUCb <vitalik560@yandex.ru>
2016-04-01 20:30:14 +0300

    add example

---
+++ Syslog-Example.c
@@ -0,0 +1,15 @@
+#include <syslog.h>
+
+int main()
+{
+    setlogmask (LOG_UPTO (LOG_NOTICE));
+
+    openlog ("exampleprog", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+
+    syslog (LOG_NOTICE, "Program started by User %d", getuid ());
+    syslog (LOG_INFO, "A tree falls in a forest");
+
+    closelog ();
+    
+    return 0;
+}
